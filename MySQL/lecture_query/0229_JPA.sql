69d7ce8c0cf6327186d8891c3f47ccab1e0e0bbe
chanchanwoong <cksdnddlek99@gmail.com>
2024-02-29 19:50:50 +0900

    Add: MySQL Query

---
+++ MySQL/lecture_query/0229_JPA.sql
@@ -0,0 +1,10 @@
+show databases;
+use ssgdb;
+
+show tables;
+desc member_table;
+desc cart;
+desc product;
+
+select * from member_table;
+insert into member_table(id,address,email,name,password,phone,gender) values(1,'1','1','1','1','1',1);
