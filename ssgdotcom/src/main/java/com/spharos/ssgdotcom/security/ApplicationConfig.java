f584cdfd7507a8c84c7dfe9f4f7fe2094b857530
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-07 11:28:05 +0900

    Add: 03.07 필터 생성

--- ssgdotcom/src/main/java/com/spharos/ssgdotcom/security/ApplicationConfig.java
+++ ssgdotcom/src/main/java/com/spharos/ssgdotcom/security/ApplicationConfig.java
@@ -2,10 +2,17 @@ package com.spharos.ssgdotcom.security;
 
 import com.spharos.ssgdotcom.member.infrastructure.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration      // 빈 등록
 @RequiredArgsConstructor
@@ -15,5 +22,29 @@ public class ApplicationConfig {
     public UserDetailsService memberDetailsService() {
         return loginId -> (UserDetails) memberRepository.findByLoginId(loginId)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found : {}" + loginId));
+    }
+
+    @Bean
+    // 그대로 가져와서 사용하는 메서드
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(memberDetailsService());
+
+        // 비밀번호 암호화 작업
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    // 기본 인증 관리자를 가져와 처리한다.
+    // 그대로 가져와서 사용하는 메서드
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // 비밀번호 암호화하는 메서드
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
